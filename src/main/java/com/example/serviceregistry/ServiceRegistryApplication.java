c76bdcbf7af3189815e8be1818429255a03264c7
yachoo2606 <janbialy@hotmail.com>
2023-12-17 15:15:37 +0100

    added the ability to send logs to the ELK stack with the ability to distinguish each node from the Eureka cluster

--- src/main/java/com/example/serviceregistry/ServiceRegistryApplication.java
+++ src/main/java/com/example/serviceregistry/ServiceRegistryApplication.java
@@ -4,12 +4,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import java.net.InetAddress;
+
 @SpringBootApplication
 @EnableEurekaServer
 public class ServiceRegistryApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("CONTAINER_IP", getHostAddress());
 		SpringApplication.run(ServiceRegistryApplication.class, args);
 	}
+
+	public static String getHostAddress() {
+        try {
+            return InetAddress.getLocalHost().getHostAddress();
+        } catch (Exception e) {
+            return "unknown";
+        }
+    }
 
 }
